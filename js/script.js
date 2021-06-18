cf48dbbfff7683f5d24e58411085811deb38d24f
Piero <piero.onorati@virgilio.it>
2021-06-18 16:46:55 +0200

    add forEach

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ La funzione ritornerà un nuovo array con i valori che hanno la posizione compre
 Usiamo i nuovi metodi degli array foreach o filter.*/
 
 
-/* METODO FILTER */
+/*--------- METODO FILTER --------- */
 
 const filterNumbers = (array, a , b) => {
     let newarray = array.filter((element, index)=>{
@@ -16,7 +16,6 @@ const filterNumbers = (array, a , b) => {
     return newarray
 }
 
-
 const numbers = [2,11,6,77,3,4,9];
 let from = 1
 let to = 3
@@ -24,3 +23,25 @@ let to = 3
 const newNumber = filterNumbers (numbers, 1, 3)
 
 console.log(newNumber);
+
+
+/*--------- METODO FOREACH --------- */
+
+const numbers2 = [4,17,9,44,54,2,7]
+
+const forEachNumbers = ( array, a , b) => {
+
+    newArray=[];
+
+    array.forEach((element, index) =>{
+        if ( index >= a-1 && index < b){
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
+}
+
+const newnumbers2 = forEachNumbers (numbers2, 1, 3);
+
+console.log(newnumbers2);
