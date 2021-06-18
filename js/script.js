8763d58541c061fda19838f6183ad707c9cfb08d
Piero <piero.onorati@virgilio.it>
2021-06-18 16:13:42 +0200

    add filter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 /* Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter.*/
+
+
+/* METODO FILTER */
+
+const filterNumbers = (array, a , b) => {
+    let newarray = array.filter((element, index)=>{
+        if ( index >= a -1 && index < b){
+           return element
+        }
+    
+    });
+
+    return newarray
+}
+
+
+const numbers = [2,11,6,77,3,4,9];
+let from = 1
+let to = 3
+
+const newNumber = filterNumbers (numbers, 1, 3)
+
+console.log(newNumber);
